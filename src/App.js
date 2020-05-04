bdc41d5c5bf7213b8f856d2854e89022047fff8d
Rumyana Dimitrieva <rumyanamm@gmail.com>
2020-05-04 11:16:25 -0700

    Setting up the project structure

--- src/App.js
+++ src/App.js
@@ -1,19 +1,18 @@
 import React, { Component } from 'react';
-import { Navbar, NavbarBrand } from 'reactstrap';
+import { BrowserRouter } from 'react-router-dom';
+import Main from './components/MainComponent';
 import './App.css';
 
 class App extends Component {
     render() {
         return (
-            <div className="App">
-                <Navbar dark color="info">
-                <div className="container">
-                    <NavbarBrand href="/">Recipes</NavbarBrand>
+            <BrowserRouter>
+                <div className="App">
+                    <Main />
                 </div>
-                </Navbar>
-            </div>
+            </BrowserRouter>
         );
-    }
+    };
 }
 
 export default App;
---
+++ src/components/FooterComponent.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+function Footer(props) {
+    return (
+        <footer className="site-footer">
+            <div className="container">
+                <div className="row justify-content-center">             
+                    <div className="col text-center">
+                        <h5>Social</h5>
+                        <a className="btn btn-social-icon btn-instagram" href="http://instagram.com/"><i className="fa fa-instagram" /></a>{' '}
+                        <a className="btn btn-social-icon btn-facebook" href="http://www.facebook.com/"><i className="fa fa-facebook" /></a>{' '}
+                        <a className="btn btn-social-icon btn-twitter" href="http://twitter.com/"><i className="fa fa-twitter" /></a>{' '}
+                        <a className="btn btn-social-icon btn-google" href="http://youtube.com/"><i className="fa fa-youtube" /></a> 
+                    </div>
+                </div>
+            </div>
+        </footer>
+    )
+}
+
+export default Footer;
